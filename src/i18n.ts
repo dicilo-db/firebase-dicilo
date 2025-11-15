10e42d8a4e9b50577c38f5beb8c2af0309fd3c1e
MILENIUM HOLDING <mhc.int.com@gmail.com>
2025-11-15 02:08:28 +0000

    un error: Unhandled Runtime Error ReferenceError: _NON_ESCAPED_CONTENT_M

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,1 +1,50 @@
-_NON_ESCAPED_CONTENT_MARKER_2
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Import translation files
+import common_de from './locales/de/common.json';
+import common_en from './locales/en/common.json';
+import common_es from './locales/es/common.json';
+import admin_de from './locales/de/admin.json';
+import admin_en from './locales/en/admin.json';
+import admin_es from './locales/es/admin.json';
+
+const resources = {
+  de: {
+    common: common_de,
+    admin: admin_de,
+  },
+  en: {
+    common: common_en,
+    admin: admin_en,
+  },
+  es: {
+    common: common_es,
+    admin: admin_es,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'de',
+    debug: false, // Set to true to see logs in console
+    interpolation: {
+      escapeValue: false, // React already safes from xss
+    },
+    detection: {
+      order: ['queryString', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+      caches: ['cookie'],
+    },
+    defaultNS: 'common',
+    react: {
+      useSuspense: false,
+      transKeepHTMLValues: true, // Preserve HTML tags in translations
+    },
+  });
+
+export default i18n;
